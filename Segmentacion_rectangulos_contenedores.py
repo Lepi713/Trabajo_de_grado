30eac6b6f46703a1c30e56b1d793263002b17b02
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2024-04-30 06:51:29 -0500

    Segmentacion_rectangulos_V2
    Se cambio para que cuando el rectangulo es hecho al revez, y la coordenada de la derecha es mas pequeña que la izquierda esta son invertidas para procesar la imagen. Se hace por medio de una prueba logica para saber cual es mayor

--- Segmentacion_rectangulos_contenedores.py
+++ Segmentacion_rectangulos_contenedores.py
@@ -1,30 +1,41 @@
 #Separacion de los rectangulos contenedeores para la generacion de la mascara de la imagen para entrenamiento
 
-from PIL import Image, ImageDraw
+from PIL import Image
 
-def extract_rectangles(input_file, output_file):
+def extract_rectangles(input_file, image_file, output_file):
+    # Open the input image
+    input_image = Image.open(image_file)
+
     # Create a black image
-    width, height = 1600, 1300  # Adjust this as per your requirement
-    image = Image.new("RGB", (width, height), color="black")
-    draw = ImageDraw.Draw(image)
+    width, height = input_image.size
+    output_image = Image.new("RGB", (width, height), color="black")
 
-    # Open the input file containing rectangle coordinates
+    # Iterate over each rectangle in the input file
     with open(input_file, "r") as file:
         lines = file.readlines()
+        for line in lines:
+            # Extract coordinates
+            x1, y1, x2, y2 = map(int, line.strip().split(","))
 
-    # Iterate over each line containing rectangle coordinates
-    for line in lines:
-        # Extract coordinates
-        x1, y1, x2, y2 = map(int, line.strip().split(","))
-
-        # Draw rectangle on the image
-        draw.rectangle([x1, y1, x2, y2], outline="white")
+            # Ensure x1 < x2 and y1 < y2
+            if x2 < x1:
+                x1, x2 = x2, x1
+            if y2 < y1:
+                y1, y2 = y2, y1
+            
+            # Extract pixels from the input image
+            region = input_image.crop((x1, y1, x2, y2))
+            
+            # Paste the pixels onto the output image
+            output_image.paste(region, (x1, y1))
 
     # Save the image with rectangles drawn
-    image.save(output_file)
-    print("Image with rectangles saved as", output_file)
+    output_image.save(output_file)
+    print("Image with extracted rectangles saved as", output_file)
 
 if __name__ == "__main__":
-    input_file = "rectangle_coordinates.txt"
-    output_file = "image_with_rectangles.png"
-    extract_rectangles(input_file, output_file)
+    input_file = r"E:\Tesis\Para entrenamiento\Salida\Coordenadas\1_100_0020_1.txt"
+    image_file = r"E:\Tesis\Para entrenamiento\Fotos_entrada\1_100_0020.JPG"
+    output_file = r"E:\Tesis\Para entrenamiento\Salida\image_with_rectangles.png"
+    extract_rectangles(input_file, image_file, output_file)
+
